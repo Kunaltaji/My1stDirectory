0cf69bfdfff8652c3935e20b86474c7eec5ef14e
JaK_theCoder <kunal101251@gmail.com>
2023-12-11 23:31:34 +0530

    modified

---
+++ Projects/My Practice code/Js_tutorial.js
@@ -0,0 +1,18 @@
+// let obj={
+//     harry:98,rohan:70,akash:75
+// }
+// for (let a in obj){
+//     console.log("marks of the "+a+" are "+obj[a]);
+// }
+// const cn = 4;
+// let i
+// while (i != cn){
+//     i=prompt("Enter the correct number...")
+//     alert("It is a wrong number, try again...")
+// }
+
+// write a function to find mean of 5 numbers...
+const meanOfFive=(a,b,c,d,e)=>{
+    return (a+b+c+d+e)/5;
+}
+meanOfFive(5,6,7,8,9)
